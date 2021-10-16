7f9d2922c3c9943b7b58e99cc8ccb0d8af4b94c5
John Lapeyre <jlapeyre@users.noreply.github.com>
2021-10-15 20:24:26 -0400

    Remove unnecessary data structure files and add tests
    * Previously several files from DataStructures.jl were copied into this repo.
    With this PR several are removed and a dependency on DataStructures.jl is
    introduced.
    
    * test/runtests.jl is created with a few tests

--- src/SweepContractor.jl
+++ src/SweepContractor.jl
@@ -6,10 +6,7 @@ module SweepContractor
 
     import Base: By, Lt, Ordering, Forward, ForwardOrdering, ReverseOrdering, lt
     include("sorted_data_structs/data_structures.jl")
-    include("sorted_data_structs/balanced_tree.jl")
     include("sorted_data_structs/avl_tree.jl")
-    include("sorted_data_structs/sorted_dict.jl")
-    include("sorted_data_structs/priorityqueue.jl")
     import LinearAlgebra
     import Base: issorted, sortperm, sort!, isless, intersect
     # Utilities to make sure networks are suitably formatted for sweep contraction
